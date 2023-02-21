010eb568e9fe5420ce9f08fdf6731a9953fd2a3d
Khoa <khoadinh2k@gmail.com>
2023-02-20 18:08:07 -0600

    implemented nav bar and hero section

--- app/page.tsx
+++ app/page.tsx
@@ -6,86 +6,34 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <body className={`${inter.className} bg-white h-full`}>
+      <header className={'flex px-10 z-10 fixed top-0 w-full h-24 items-center'}>
+        <nav className={'flex align-middle w-full justify-between'}>
+          <div className={'p-2'}>{'Home'}</div>
+          <ol className={'flex justify-end'}>
+            <li className={'p-2'}>About</li>
+            <li className={'p-2'}>Experience</li>
+            <li className={'p-2'}>Work</li>
+            <li className={'p-2'}>Contact</li>
+          </ol>
+        </nav>
+      </header>
+      <div id={'content'}>
+        <section className={'mx-auto my-0 w-full max-w-screen-2xl px-6 sm:px-12 lg:px-36 min-h-screen h-screen'}>
+          <div className={'mx-auto max-w-5xl flex h-screen justify-center flex-col'}>
+            <div>
+              <h1 className={'pb-2'}>Hello, my name is</h1>
+            </div>
+            <div>
+              <h2 className={'text-4xl font-semibold'}>Khoa Dinh.</h2>
+            </div>
+            <div>
+              <h3>Frontend Dev</h3>
+            </div>
+          </div>
+        </section>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </body>
   )
 }
